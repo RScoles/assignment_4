dabe4c19d0b141b687b819caefe80f82bdd72736
RScoles <94501330+RScoles@users.noreply.github.com>
2023-06-05 17:46:04 -0500

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,15 @@
+-- Part 1: Test it with SQL
+//employer varchar
+//id int
+//name varchar
+//job varchar
+-- Part 2: Test it with SQL
+SELECT name FROM employer WHERE location = "St. Louis City";
+-- Part 3: Test it with SQL
+DROP TABLE job;
+-- Part 4: Test it with SQL
+SELECT *
+FROM skill
+LEFT JOIN job_skills ON job_skills.skills_id = skill.id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
---
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -0,0 +1,87 @@
+package org.launchcode.techjobs.persistent.controllers;
+
+import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+public class HomeController {
+
+
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+    @Autowired
+    private JobRepository jobRepository;
+
+    @RequestMapping("")
+    public String index(Model model) {
+
+        model.addAttribute("title", "My Jobs");
+
+        return "index";
+    }
+
+    @GetMapping("add")
+    public String displayAddJobForm(Model model) {
+        model.addAttribute("title", "Add Job");
+        model.addAttribute(new Job());
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
+        return "add";
+    }
+
+    @PostMapping("add")
+    public String processAddJobForm(@ModelAttribute @Valid Job newJob,
+                                       Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Job");
+            return "add";
+        }
+
+        Optional<Employer> result = employerRepository.findById(employerId);
+        if (result.isEmpty()) {
+            model.addAttribute("title", "Invalid Category ID: " + employerId);
+        } else {
+            Employer employer = result.get();
+            newJob.setEmployer(employer);
+        }
+        Optional<List<Skill>> skillObjs = Optional.of((List<Skill>) skillRepository.findAllById(skills));
+
+        if(skillObjs.isPresent()){
+            List<Skill> newSkills = skillObjs.get();
+            newJob.setSkills(newSkills);
+        }
+
+        jobRepository.save(newJob);
+
+        return "redirect:";
+    }
+
+    @GetMapping("view/{jobId}")
+    public String displayViewJob(Model model, @PathVariable int jobId) {
+
+        return "view";
+    }
+
+
+}
