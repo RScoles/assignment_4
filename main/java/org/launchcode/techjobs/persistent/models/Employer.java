519e9f847fc94bda25e47c0fbf8b882511b92b55
RScoles <94501330+RScoles@users.noreply.github.com>
2023-06-10 09:26:10 -0500

    Might be finished

---
+++ main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -0,0 +1,42 @@
+package org.launchcode.techjobs.persistent.models;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Employer extends AbstractEntity {
+
+    @NotBlank
+    @Size(max = 100,message = "Location can have max of 100 characters")
+    private String location;
+
+@OneToMany
+@JoinColumn(name = "employer_id")
+private List<Job> jobs = new ArrayList<>();
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+
+    }
+
+
+
+    public String getLocation() {
+    return location;
+}
+
+public void setLocation(String location) {
+    this.location = location;
+    }
+    public Employer(){}
+}
---
+++ main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -0,0 +1,44 @@
+package org.launchcode.techjobs.persistent.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Job extends AbstractEntity {
+
+    @ManyToOne
+    private Employer employer;
+    @ManyToMany
+    private List<Skill> skills = new ArrayList<>();
+
+    public Job() {
+    }
+
+    public Job(Employer anEmployer, List<Skill> theseSkills) {
+        super();
+        this.employer = anEmployer;
+        this.skills = theseSkills;
+    }
+
+    // Getters and setters.
+
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+
+}
